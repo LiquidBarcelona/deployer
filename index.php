aedf9c827c793816bcdee1549b64bae2d5a4315f
Marc Bernet <mbernet@gmail.com>
2016-03-02 15:42:49 +0100

    test

--- index.php
+++ index.php
@@ -3,3 +3,8 @@ include_once('config/config.php');
 include_once('config/repositories.php');
 include_once('classes/object.php');
 include_once('classes/deploy.php');
+
+$deployer = new Deployer();
+$deployer->deploy();
+
+echo 'ok';
