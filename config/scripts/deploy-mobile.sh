a74db63f34974f090f8b771e68a2f8c6e049fc8f
Marc Bernet <mbernet@gmail.com>
2016-03-02 16:31:35 +0100

    test

--- config/scripts/deploy-mobile.sh
+++ config/scripts/deploy-mobile.sh
@@ -4,20 +4,9 @@
 LOG="/var/www/ci/logs/deeplio.log"
 NOW=$(date +"%Y-%m-%d-%H%M%S")
 ## Start script:
-echo "
---------------------------------------------------------
-NEW DEPLOYMENT: $NOW" >> $LOG
+
 cd /var/www/mobile-shopify/
-echo "Changing Dir"
-git checkout devel >> $LOG
+git checkout devel
 ## Clear untracked files:
-git clean -df >> $LOG
-## Revert local changes:
-git checkout -- . >> $LOG
-git pull >> $LOG
-
-# Copy some stuff around:
-#cp $DEEPLIO/index.html $DEPLOY/
-#cp $DEEPLIO/CHANGELOG.md $DEPLOY/
-
-echo "End Deploy." >> $LOG
+git clean -df
+git pull
